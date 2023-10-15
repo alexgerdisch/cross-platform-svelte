626d894e6f70602c0330b7c9d106bc811e88f686
alexgerdisch <alexgerdisch@gmail.com>
2023-10-14 22:42:56 -0700

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let recorder;
+  let player;
+</script>
+
+<h1>ClimbUp</h1>
+<h2>Upload your vid</h2>
+<input bind:this={recorder} type="file" accept="video/*" capture />
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={player} id="player" controls />
